bf15a5945880dc285d4de293c95c9d3ed28bd201
NZpatelK <patelk.dev@hotmail.com>
2024-08-15 15:04:08 +1200

    add: responsive feature.

---
+++ src/useResponsiveTSX.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+
+
+export const useResponsiveTSX = (breakpoints: number[]) => {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const updateIndex = () => {
+            const width = window.innerWidth;
+            const newIndex = breakpoints.findIndex((bp) => width <= bp);
+            setIndex(newIndex === -1 ? breakpoints.length : newIndex);
+        };
+
+        updateIndex();
+        window.addEventListener("resize", updateIndex);
+        return () => window.removeEventListener("resize", updateIndex);
+
+    }, [breakpoints]);
+
+    return index;
+
+}
